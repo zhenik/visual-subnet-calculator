5aaab70e924fb3f484d34dd59afc81d1fdf8991b
sergei <ravinski.sergei@gmail.com>
2025-04-24 22:08:30 +0200

    Horizontal_line_dark styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,28 +4,33 @@ import "./globals.css";
 import ThemeRegistry from "@/themeRegistry"; // Import the new hydration wrapper
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Visual Subnet Calculator",
-    description: "Next.js-based Subnet Calculator",
+	title: "Visual Subnet Calculator",
+	description: "Next.js-based Subnet Calculator",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeRegistry> {/* Wrap app with MUI hydration wrapper */}
-            {children}
-        </ThemeRegistry>
-        </body>
-        </html>
-    );
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<ThemeRegistry>
+					{/* Wrap app with MUI hydration wrapper */}
+					{children}
+				</ThemeRegistry>
+			</body>
+		</html>
+	);
 }
